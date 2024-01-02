8e75d9f0399b960fb88a50c882847c344293a4bf
mbozkayaGitHub <mbozkaya27@gmail.com>
2024-01-02 08:01:55 +0300

    Gif&Image_added

---
+++ src/helper/projectData.js
@@ -0,0 +1,13 @@
+
+import restaurant from "../assets/img/Animation.gif"
+import hospital from "../assets/img/AppointmentApp.gif"
+import StyledComponent from "../assets/img/StyledcomponentApp.gif"
+
+
+
+
+export const projectData = [
+    {name:"Restaurant",image: restaurant, techTool:"React | Nextjs | Redux | MongoDB |",githubLink:"https://soup-restaurant.vercel.app/",liveLink:"https://soup-restaurant.vercel.app/" },
+    {name:"Hospital",image: hospital, techTool:"React | Nextjs | Redux | MongoDB",githubLink:"https://soup-restaurant.vercel.app/",liveLink:"https://appoinment-project.vercel.app/" },
+    {name:"Huddle",image: StyledComponent, techTool:"React | Nextjs | Redux | MongoDB",githubLink:"https://github.com/mbozkayaGitHub/Styledcomponentproject",liveLink:"https://styledcomponentproject.vercel.app/" }
+]
